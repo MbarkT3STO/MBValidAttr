432dc1d701cbb57276b57604c132e9df2d1fec10
MBARK T3STO <mbarktiesto@outlook.com>
2021-06-27 17:14:26 +0100

    Added = > DateShouldNotEqual & TimeShouldNotEqual

---
+++ MBValidAttr/Validation Attributes/Date/DateShouldNotEqual.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MBValidAttr.Validation_Attributes.Date
+{
+    /// <summary>
+    /// Ensuring that the value of the property is not equals to a given DATE
+    /// </summary>
+    public class DateShouldNotEqual : ValidationAttribute
+    {
+        private string _valueToBeChecked;
+        private string _errorMessage;
+
+
+        /// <param name="valueToBeChecked">Value to be checked</param>
+        /// <param name="errorMessage">Error message if the property's value is equal to <paramref name="valueToBeChecked"/></param>
+        public DateShouldNotEqual(string valueToBeChecked, string errorMessage)
+        {
+            _valueToBeChecked= valueToBeChecked;
+            _errorMessage = errorMessage;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+           //return (Convert.ChangeType(_valueToBeChecked, value.GetType()) == Convert.ChangeType(value, value.GetType())) 
+           return (Convert.ToDateTime(_valueToBeChecked) == Convert.ToDateTime(value)) 
+                ? new ValidationResult(_errorMessage)
+                : ValidationResult.Success;
+        }
+    }
+}
---
+++ MBValidAttr/Validation Attributes/Time/TimeShouldNotEqual.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MBValidAttr.Validation_Attributes.Time
+{
+    /// <summary>
+    /// Ensuring that the value of the property is not equals to a given TIME
+    /// </summary>
+    public class TimeShouldNotEqual : ValidationAttribute
+    {
+        private string _valueToBeChecked;
+        private string _errorMessage;
+
+
+        /// <param name="valueToBeChecked">Value to be checked</param>
+        /// <param name="errorMessage">Error message if the property's value is equal to <paramref name="valueToBeChecked"/></param>
+        public TimeShouldNotEqual(string valueToBeChecked, string errorMessage)
+        {
+            _valueToBeChecked = valueToBeChecked;
+            _errorMessage = errorMessage;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            return ( TimeSpan.Parse(_valueToBeChecked) == TimeSpan.Parse(value.ToString()))
+                 ? new ValidationResult(_errorMessage)
+                 : ValidationResult.Success;
+        }
+    }
+}
