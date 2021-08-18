924b5c1b37a1e9ac40d7c85d3832af3c6758b3b2
MBARK T3STO <mbarktiesto@outlook.com>
2021-08-18 16:50:28 +0100

    Added => ShouldBetween : Method

---
+++ MBValidAttr/Validation Attributes/Number/ShouldBetween.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+
+namespace MBValidAttr.Validation_Attributes.Number
+{
+    /// <summary>
+    /// Ensure that the value of the property is between two given values
+    /// </summary>
+    public class ShouldBetween : ValidationAttribute
+    {
+        private readonly decimal _minValue;
+        private readonly decimal _maxValue;
+        private readonly string  _errorMessage;
+
+        /// <summary>
+        /// Ensure that the value of the property is between <see cref="minValue"/> and <see cref="maxValue"/>
+        /// </summary>
+        /// <param name="minValue">Min value</param>
+        /// <param name="maxValue">Max value</param>
+        /// <param name="errorMessage">Error message if the property's value Less than Min And/Or Greater than Max</param>
+        public ShouldBetween(object minValue, object maxValue, string errorMessage)
+        {
+            _minValue     = Convert.ToDecimal( minValue );
+            _maxValue     = Convert.ToDecimal( maxValue );
+            _errorMessage = errorMessage;
+        }
+
+        protected override ValidationResult IsValid( object value , ValidationContext validationContext )
+        {
+            var currentValueAsDecimal = Convert.ToDecimal( value );
+
+            //// Convert the Max Value to the VALUE(Current property) type
+            //var minValue = Convert.ChangeType( _minValue , value.GetType() );
+            
+            //// Convert the Max Value to the VALUE(Current property) type
+            //var maxValue = Convert.ChangeType( _maxValue , value.GetType() );
+
+            return currentValueAsDecimal >= _minValue && currentValueAsDecimal <= _maxValue
+                       ? ValidationResult.Success
+                       : new ValidationResult( _errorMessage );
+        }
+    }
+}
--- MBValidAttr/Validation Attributes/Number/ShouldGreaterThanOrEqual.cs
+++ MBValidAttr/Validation Attributes/Number/ShouldGreaterThanOrEqual.cs
@@ -1,20 +1,20 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 
-namespace MBValidAttr.Validation_Attributes
+namespace MBValidAttr.Validation_Attributes.Number
 {
     /// <summary>
-    /// Ensuring that the value of the property is greater than a given value
+    /// Ensuring that the value of the property is greater than or equal to a given value
     /// </summary>
-    public class ShouldGreaterThan : ValidationAttribute
+    public class ShouldGreaterThanOrEqual : ValidationAttribute
     {
         private readonly dynamic _valueToBeChecked;
         private readonly string _errorMessage;
 
 
         /// <param name="valueToBeChecked">Value to be checked</param>
-        /// <param name="errorMessage">Error message if the property's value less than or equal to <paramref name="valueToBeChecked"/></param>
-        public ShouldGreaterThan(object valueToBeChecked, string errorMessage)
+        /// <param name="errorMessage">Error message if the property's value less than <paramref name="valueToBeChecked"/></param>
+        public ShouldGreaterThanOrEqual(object valueToBeChecked, string errorMessage)
         {
             _valueToBeChecked = valueToBeChecked;
             _errorMessage = errorMessage;
@@ -23,7 +23,7 @@ namespace MBValidAttr.Validation_Attributes
 
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            return (Convert.ChangeType(value, value.GetType()) > Convert.ChangeType(_valueToBeChecked, value.GetType()))
+            return (Convert.ChangeType(value, value.GetType()) >= Convert.ChangeType(_valueToBeChecked, value.GetType()))
                 ? ValidationResult.Success
                 : new ValidationResult(_errorMessage);
         }
