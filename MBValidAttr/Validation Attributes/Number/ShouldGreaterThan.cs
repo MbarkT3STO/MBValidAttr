96bc4fa9e118f647f0b198ccb92d684c77c9b1ad
MBARK T3STO <mbarktiesto@outlook.com>
2022-03-15 19:23:58 +0100

    Modified => ShouldGreaterThan : Support (Decimal, Int, Byte and Double ) types

--- MBValidAttr/Validation Attributes/Number/ShouldGreaterThan.cs
+++ MBValidAttr/Validation Attributes/Number/ShouldGreaterThan.cs
@@ -8,24 +8,46 @@ namespace MBValidAttr.Validation_Attributes.Number
     /// </summary>
     public class ShouldGreaterThan : ValidationAttribute
     {
-        private readonly dynamic _valueToBeChecked;
+        private readonly decimal _valueToBeChecked;
         private readonly string _errorMessage;
 
 
         /// <param name="valueToBeChecked">Value to be checked</param>
         /// <param name="errorMessage">Error message if the property's value less than or equal to <paramref name="valueToBeChecked"/></param>
-        public ShouldGreaterThan(object valueToBeChecked, string errorMessage)
+        public ShouldGreaterThan(byte valueToBeChecked, string errorMessage)
         {
             _valueToBeChecked = valueToBeChecked;
-            _errorMessage = errorMessage;
+            _errorMessage     = errorMessage;
+        }   
+        
+        /// <param name="valueToBeChecked">Value to be checked</param>
+        /// <param name="errorMessage">Error message if the property's value less than or equal to <paramref name="valueToBeChecked"/></param>
+        public ShouldGreaterThan(int valueToBeChecked, string errorMessage)
+        {
+            _valueToBeChecked = valueToBeChecked;
+            _errorMessage     = errorMessage;
+        }  
+        
+        /// <param name="valueToBeChecked">Value to be checked</param>
+        /// <param name="errorMessage">Error message if the property's value less than or equal to <paramref name="valueToBeChecked"/></param>
+        public ShouldGreaterThan(double valueToBeChecked, string errorMessage)
+        {
+            _valueToBeChecked = ( decimal ) valueToBeChecked;
+            _errorMessage     = errorMessage;
+        }     
+        
+        /// <param name="valueToBeChecked">Value to be checked</param>
+        /// <param name="errorMessage">Error message if the property's value less than or equal to <paramref name="valueToBeChecked"/></param>
+        public ShouldGreaterThan(decimal valueToBeChecked, string errorMessage)
+        {
+            _valueToBeChecked = valueToBeChecked;
+            _errorMessage     = errorMessage;
         }
 
 
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            return (Convert.ChangeType(value, value.GetType()) > Convert.ChangeType(_valueToBeChecked, value.GetType()))
-                ? ValidationResult.Success
-                : new ValidationResult(_errorMessage);
+            return Convert.ToDecimal(value) > _valueToBeChecked ? ValidationResult.Success : new ValidationResult( _errorMessage );
         }
     }
 }
